656bd66e67ea1245ebae5f1456c3b23870108df9
Anithasivakumar <37569380+Anithasivakumar@users.noreply.github.com>
2019-07-24 15:34:27 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,8 @@
+n=str(input(""))
+if(n=='A'or n=='a' or n=='E'or n=='e' or n=='I'or n=='i' or n=='O'or n=='o' or
+   n=='U'or n=='u'):
+    print("Vowel")
+elif((n>='a'and n<='z') or (n>='A'and n<='Z')):
+     print("Consonant")
+else:
+     print("invalid")
